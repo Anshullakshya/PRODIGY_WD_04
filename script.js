6e6d49c9761918dc037e217194473442345205e5
Anshul Lakshya <157568186+Anshullakshya@users.noreply.github.com>
2024-04-14 22:23:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// This script is optional if you want to add smooth scrolling behavior.
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
